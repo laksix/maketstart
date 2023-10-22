b437cd23e1deba200164f9797a3a59e7b4b18e1e
laksix <147379934+laksix@users.noreply.github.com>
2023-10-22 21:17:54 +0700

    Add files via upload

---
+++ slider/app1.js
@@ -0,0 +1,59 @@
+
+
+
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  spaceBetween: -16,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
+});
+
+window.onresize = function() {
+  if (window.innerWidth > 767) {
+    swiper.disable();
+  } else {
+    swiper.enable();
+  }
+};
+
+
+
+// КНОПКА раскрытия на планшет //
+
+const flipImgRename = document.querySelector(".main-info__button");
+flipImgRename.addEventListener ("click",() => {
+  const flipImg = document.querySelector (".main-info__img-more");
+  flipImg.classList.toggle("active-img");
+  const Rename = document.querySelector (".main-info__button-text");
+  Rename.classList.toggle ("button-active");
+  if (Rename.classList.contains("button-active")){
+    Rename.innerHTML = "Скрыть";
+    const addNewCompany = document.querySelectorAll(".slider__item");
+    for (i = 8; i < addNewCompany.length;i++){
+      addNewCompany[i].classList.remove("disable-pc")
+    }
+    for (i = 6;i < addNewCompany.length; i++){
+      addNewCompany[i].classList.remove("disable")
+    }
+
+  } else {
+    Rename.innerHTML = "Показать все";
+    const addNewCompany = document.querySelectorAll(".slider__item");
+    for (i = 6;i < addNewCompany.length; i++){
+      addNewCompany[i].classList.add("disable")
+    }
+    for (i = 8; i < addNewCompany.length;i++){
+      addNewCompany[i].classList.add("disable-pc")
+    }
+  };
+ 
+
+
+})
+
+
+
